0c89b971dd51fffe2a14ebbc8b0ed260d0c09185
liuwenxu1993 <liuwenxu121380@163.com>
2019-08-02 09:32:03 +0800

    springcloud

---
+++ src/test/java/test/TestArrayAndString.java
@@ -0,0 +1,10 @@
+package test;
+
+public class TestArrayAndString {
+	public static void main(String[] args) {
+		String a1 = "hello";
+		String a2 = "hello";
+		final String a3 = "lo";
+		System.out.println(a1==a2);
+	}
+}	
